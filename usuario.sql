d4e6cd2787ebf7823a9f651b5b306c8d47f5e955
Allison Rodrigues <allison_rps@hotmail.com>
2023-10-30 20:58:20 -0300

    alteração tabela usuario.sql

--- usuario.sql
+++ usuario.sql
@@ -2,6 +2,7 @@ CREATE TABLE usuario (
     id INT AUTO_INCREMENT PRIMARY KEY,
     nomeUser VARCHAR(255) NOT NULL,
     senha VARCHAR(255) NOT NULL,
+    senhaSegura VARCHAR(255) NOT NULL,
     nome VARCHAR(255) NOT NULL,
     email VARCHAR(255) NOT NULL,
     dataNascimento DATE,
